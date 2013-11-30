f855e183188c3f64ab235a1c1b7370bad772e161
Malte Vollmerhausen <maltevol@gmail.com>
2013-11-30 15:51:10 +0100

    Android test code

--- HackathonAndroid/src/com/Swarm/TestActivity.java
+++ HackathonAndroid/src/com/Swarm/TestActivity.java
@@ -2,8 +2,26 @@ package com.Swarm;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 import com.example.HackathonAndroid.R;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.StatusLine;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,11 +36,41 @@ public class TestActivity extends Activity {
 
         setContentView(R.layout.test_layout);
 
-        TextView tv = (TextView) findViewById(R.id.textView);
+        final TextView tv = (TextView) findViewById(R.id.textView);
 
         tv.setText("Hello World!");
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("button clicked!");
+                String response = null;
+                try {
+                    tv.setText("Response: "+postData());
+                } catch (IOException e) {
+                    e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                }
+            }
+        });
 
 
+    }
+
+    public String postData() throws IOException {
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpResponse response = null;
+
+        response = httpclient.execute(new HttpGet("http://hdm-jssm.appspot.com/"));
+        StatusLine statusLine = response.getStatusLine();
+        if(statusLine.getStatusCode() == HttpStatus.SC_OK){
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            response.getEntity().writeTo(out);
+            out.close();
+            return out.toString();
+        } else{
+            //Closes the connection.
+            response.getEntity().getContent().close();
+            throw new IOException(statusLine.getReasonPhrase());
+        }
 
     }
 }
